33e99036e826e39ee92ac5abb4d054f028ea4048
rosechellejoy <rosechellejoyoraa@gmail.com>
2015-08-20 14:51:12 +0800

    added week2

---
+++ controllers/teacher.js
@@ -0,0 +1,15 @@
+exports.view=function(req,res){
+	res.send('View Teachers');
+};
+
+exports.add=function(req,res){
+	res.send('Add Teachers');
+};
+
+exports.update=function(req,res){
+	res.send('Update Teachers');
+};
+
+exports.del=function(req,res){
+	res.send('Delete Teachers');
+};
---
+++ index.js
@@ -0,0 +1,30 @@
+
+var express= require('express');
+var app= express();
+
+var server = app.listen(5000, function(){
+	var host = server.address().adress;
+	var port = server.address().port;
+	console.log('Example app listening at http://%s:%s', host, port);
+	});
+
+/*app.get("/", function(req,res){
+	res.send('Hello World!');
+});
+
+app.route('/student')
+	.get(function(req,res){
+		res.send('Get a student');
+	})
+	.post(function(req,res){
+		res.send('Add a student');
+	})
+	.put(function(req,res){
+		res.send('Update a student');
+	});
+*/
+
+app.use(require('body-parser')());  //nagreread ng forms ng input , nilalagay sa req-body
+app.use(require('method-override')());  //kelangan ng put, delete
+app.use(require(__dirname+'/config/router')(express.Router()));
+
